bff00e083cf5e37b72b7b400544a8d96ab6603b4
Yannick Dueren <yannick.dueren@gmail.com>
2026-05-15 18:59:46 +0200

    Bump version to 3.4.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,8 +22,8 @@ try:
     version = __version__
     release = __version__
 except ImportError:
-    version = "3.4.0"
-    release = "3.4.0"
+    version = "3.4.1"
+    release = "3.4.1"
 
 # -- General configuration ---------------------------------------------------
 
